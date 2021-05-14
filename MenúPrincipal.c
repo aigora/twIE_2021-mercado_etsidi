a99e75234b7b7aefb159171939d52b7db81d1b4c
InesMelendez <80213891+InesMelendez@users.noreply.github.com>
2021-05-14 21:50:39 +0200

    Añadido función

--- MenúPrincipal.c
+++ MenúPrincipal.c
@@ -3,6 +3,7 @@
 void menu_principal();
 void pagina_web();
 void datosenvio();
+void Fecha(); //Funcion para mostrar la hora en pantalla
 FILE  *fd; 
 
 using namespace std;
@@ -48,50 +49,60 @@ int main()
   system("Color e");
   menu_principal();
   system("cls");
-                    charcuteria = fopen("charcuteria.txt","r");
-                                if (charcuteria == NULL){
-                                    printf("Error al abrir el archivo destino.\n");
-                                    return -1;
-                                }else{
+charcuteria = fopen("charcuteria.txt","r");
+                                if (charcuteria == NULL)
+								{
+                                  printf("Error al abrir el archivo destino.\n");
+                                  return -1;
+                                }else
+								  {
                                     while(fscanf(charcuteria, "%i %s %f %f",&tienda[nproductos].orden,tienda[nproductos].tipo,&tienda[nproductos].peso,&tienda[nproductos].precio)!=EOF)
-                                        {
+                                    {
                                     nproductos++;
-                                        }
+                                    }
                                     fclose(charcuteria);
-                                }
+                                  }
+                                  
                     pescaderia = fopen("pescaderia.txt","r");
-                                if (pescaderia == NULL){
-                                    printf("Error al abrir el archivo destino.\n");
-                                    return -2;
-                                }else{
+                                if (pescaderia == NULL)
+								{
+                                  printf("Error al abrir el archivo destino.\n");
+                                  return -2;
+                                }else
+								  {
                                     while(fscanf(pescaderia, "%i %s %f %s %f",&tienda1[nproductos1].orden,tienda1[nproductos1].tipo,&tienda1[nproductos1].peso,tienda1[nproductos1].unidad_peso,&tienda1[nproductos1].precio)!=EOF)
-                                        {
+                                    {
                                     nproductos1++;
-                                        }
+                                    }
                                     fclose(pescaderia);
-                                }
+                                  }
+                                  
                     panaderia = fopen("panaderia.txt","r");
-                                if (panaderia == NULL){
-                                    printf("Error al abrir el archivo destino.\n");
-                                    return -3;
-                                }else{
+                                if (panaderia == NULL)
+								{
+                                  printf("Error al abrir el archivo destino.\n");
+                                  return -3;
+                                }else
+								  {
                                     while(fscanf(panaderia, "%i %s %f",&tienda2[nproductos2].orden,tienda2[nproductos2].tipo,&tienda2[nproductos2].precio)!=EOF)
-                                        {
+                                    {
                                     nproductos2++;
-                                        }
+                                    }
                                     fclose(panaderia);
-                                }
+                                  }
                     fruteria = fopen("fruteria.txt","r");
-                                if (fruteria == NULL){
-                                    printf("Error al abrir el archivo destino.\n");
-                                    return -4;
-                                }else{
+                                if (fruteria == NULL)
+								{
+                                  printf("Error al abrir el archivo destino.\n");
+                                  return -4;
+                                }else
+								  {
                                     while(fscanf(fruteria, "%i %s %f %f",&tienda3[nproductos3].orden,tienda3[nproductos3].tipo,&tienda3[nproductos3].peso,&tienda3[nproductos3].precio)!=EOF)
-                                        {
+                                    {
                                     nproductos3++;
-                                        }
+                                    }
                                     fclose(fruteria);
-                                }
+                                  }
                     do{
                     pagina_web();
                     printf("\t\t\t\t\t-----------------------------------------\n");
@@ -101,6 +112,7 @@ int main()
                     printf ("\t\t\t\t\t\t     2.-Pescaderia\n\n");
                     printf ("\t\t\t\t\t\t     3.-Panaderia\n\n");
                     printf ("\t\t\t\t\t\t     4.-Fruteria\n\n");
+	            printf ("\t\t\t\t\t\t     5.-Salir del programa\n\n");
                     printf ("\t\t\t\t\t\t     Elige una opcion\n\n");
                     scanf ("%i",&eleccion_operacion);
                             switch (eleccion_operacion)
@@ -108,14 +120,18 @@ int main()
                             case 1:
                                 system("cls");
                                 printf("--------------------------------------\n");
-                                printf("--------------CHARCUTERIA-------------\n");
-                                printf("--------------------------------------\n\n");
+                                printf("--------------CHARCUTERIA-------------");
+                                printf("      ");
+                                Fecha();
+                                printf("\n--------------------------------------\n\n");
                                 printf("Seleccione el producto deseado\n\n");
-                                for(i=0;i<nproductos;i++){
+                                for(i=0;i<nproductos;i++)
+				{
                                 printf("%i.-%s\t\t%.2fg\t\t%.2f euros\t\n",tienda[i].orden,tienda[i].tipo,tienda[i].peso,tienda[i].precio);
-                                        }
+                                }
                                 fflush(stdin);
-                                    for (eleccion_productos=0;eleccion_productos<nproductos;eleccion_productos++){
+                                    for (eleccion_productos=0;eleccion_productos<nproductos;eleccion_productos++)
+				       {
                                             scanf("%i",&eleccion_productos);
                                             system("cls");
                                             printf ("Usted ha elegido %s\n",tienda[eleccion_productos-1].tipo);
@@ -124,20 +140,23 @@ int main()
                                             scanf ("%i",&cantidad);
                                             coste=cantidad*tienda[eleccion_productos-1].precio;
                                             printf ("La cantidad a pagar es: %.2f\n",coste);
-
-                                                    }
+                                        }
                             break;
                             case 2:
                                 system("cls");
                                 printf("--------------------------------------\n");
-                                printf("--------------PESCADERIA--------------\n");
-                                printf("--------------------------------------\n\n");
+                                printf("--------------PESCADERIA-------------");
+                                printf("      ");
+                                Fecha();
+                                printf("\n--------------------------------------\n\n");
                                 printf("Seleccione el producto deseado\n\n");
-                                for(j=0;j<nproductos1;j++){
+                                for(j=0;j<nproductos1;j++)
+				{
                                 printf("%i.-%s\t\t%.2f\t%s\t\t%.2f euros\t\n",tienda1[j].orden,tienda1[j].tipo,tienda1[j].peso,tienda1[j].unidad_peso,tienda1[j].precio);
-                                        }
+                                }
                                 fflush(stdin);
-                                for (eleccion_productos=0;eleccion_productos<nproductos1;eleccion_productos++){
+                                     for (eleccion_productos=0;eleccion_productos<nproductos1;eleccion_productos++)
+				        {
                                             scanf("%i",&eleccion_productos);
                                             system("cls");
                                             printf ("Usted ha elegido %s\n",tienda1[eleccion_productos-1].tipo);
@@ -146,20 +165,24 @@ int main()
                                             scanf ("%i",&cantidad);
                                             coste=cantidad*tienda1[eleccion_productos-1].precio;
                                             printf ("La cantidad a pagar es: %.2f\n",coste);
-                                                    }
+                                         }
 
                             break;
                             case 3:
                                 system("cls");
                                 printf("--------------------------------------\n");
-                                printf("--------------PANADERIA---------------\n");
-                                printf("--------------------------------------\n\n");
+                                printf("--------------PANADERIA-------------");
+                                printf("      ");
+                                Fecha();
+                                printf("\n--------------------------------------\n\n");
                                 printf("Seleccione el producto deseado\n\n");
-                                for(k=0;k<=nproductos2;k++){
+                                for(k=0;k<=nproductos2;k++)
+				{
                                 printf("%i.-%s\t\t%.2f euros\t\n",tienda2[k].orden,tienda2[k].tipo,tienda2[k].precio);
-                                        }
+                                }
                                 fflush(stdin);
-                                for (eleccion_productos=0;eleccion_productos<nproductos2;eleccion_productos++){
+                                     for (eleccion_productos=0;eleccion_productos<nproductos2;eleccion_productos++)
+				        {
                                             scanf("%i",&eleccion_productos);
                                             system("cls");
                                             printf ("Usted ha elegido %s\n",tienda2[eleccion_productos-1].tipo);
@@ -168,21 +191,24 @@ int main()
                                             scanf ("%i",&cantidad);
                                             coste=cantidad*tienda2[eleccion_productos-1].precio;
                                             printf ("La cantidad a pagar es: %.2f\n",coste);
-
-                                                    }
+                                        }
 
                             break;
                             case 4:
                                 system("cls");
                                 printf("--------------------------------------\n");
-                                printf("---------------FRUTERIA---------------\n");
-                                printf("--------------------------------------\n\n");
+                                printf("--------------FRUTERIA-------------");
+                                printf("      ");
+                                Fecha();
+                                printf("\n--------------------------------------\n\n");
                                 printf("Seleccione el producto deseado\n\n");
-                                for(l=0;l<=nproductos;l++){
+                                for(l=0;l<=nproductos;l++)
+				{
                                 printf("%i.-%s\t\t%.2f Kg\t\t%.2f euros\t\n",tienda3[l].orden,tienda3[l].tipo,tienda3[l].peso,tienda3[l].precio);
-                                        }
+                                }
                                 fflush(stdin);
-                                for (eleccion_productos=0;eleccion_productos<nproductos3;eleccion_productos++){
+                                     for (eleccion_productos=0;eleccion_productos<nproductos3;eleccion_productos++)
+				         {
                                             scanf("%i",&eleccion_productos);
                                             system("cls");
                                             printf ("Usted ha elegido %s\n",tienda3[eleccion_productos-1].tipo);
@@ -191,8 +217,7 @@ int main()
                                             scanf ("%i",&cantidad);
                                             coste=cantidad*tienda3[eleccion_productos-1].precio;
                                             printf ("La cantidad a pagar es: %.2f\n",coste);
-
-                                                    }
+                                         }
 
                             break;
                                 }
@@ -280,4 +305,17 @@ void datosenvio()
         fclose(clientes);
 
         }
+}
+
+void Fecha()
+{
+	time_t t;
+	struct tm *tm;
+	char fecha_dia[100];
+	
+	t = time(NULL);
+	
+	tm = localtime(&t);
+	strftime(fecha_dia, 100, "%d/%m/%Y", tm);
+	printf(" Fecha actual: %s", fecha_dia);
 }
