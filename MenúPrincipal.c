cbdfe10ea586a4da040f94ca0415ed03da06ae00
InesMelendez <80213891+InesMelendez@users.noreply.github.com>
2021-05-21 22:45:39 +0200

    Modificación función datos de envío

--- MenúPrincipal.c
+++ MenúPrincipal.c
@@ -392,7 +392,7 @@ void datosenvio()
     printf("Introduzca un telefono de contacto\n");
     scanf("%d",&datos.telefono);
     printf("Introduzca la direccion del envio\n");
-    gets(datos.direccion);
+    scanf("%s",datos.direccion);
 
 
     clientes = fopen("usuarios.txt","w");
